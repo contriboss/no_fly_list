f1f9f0f46eac2bc9376ab043aff57b4c57dd74f2
Abdelkader Boudih <terminale@gmail.com>
2024-12-19 08:27:28 +0100

    fix: no_fly_list:transformer fix

--- lib/generators/no_fly_list/transformer_generator.rb
+++ lib/generators/no_fly_list/transformer_generator.rb
@@ -8,6 +8,7 @@ require 'rails/generators/named_base'
 unless defined?(ApplicationTagTransformer)
   module NoFlyList
     module Generators
+      # bin/rails g no_fly_list:transformer
       class TransformerGenerator < Rails::Generators::Base
         source_root File.expand_path('templates', __dir__)
         def create_tag_transformer_file
