4395c3a154df2586692b74d76ce1ab342798ef03
Abdelkader Boudih <terminale@gmail.com>
2025-03-22 12:18:34 +0000

    feat: Update TaggingProxy tests to use Car model

--- test/tagging_proxy_test.rb
+++ test/tagging_proxy_test.rb
@@ -1,57 +1,67 @@
+
 # frozen_string_literal: true
 
 require "test_helper"
 
 class TaggingProxyTest < ActiveSupport::TestCase
   def setup
-    @person = Person.create!(first_name: "John", last_name: "Doe")
+    @car = Car.create!(make: "Toyota", model: "Corolla")
   end
 
   def test_add_tags
-    @person.pronouns_list.add("he/ him")
-    assert_equal ["he", "him"], @person.pronouns_list.to_a
+    @car.colors_list.add("red, blue")
+    assert_equal ["red", "blue"], @car.colors_list.to_a
   end
 
   def test_remove_tags
-    @person.pronouns_list.add("he/ him")
-    @person.pronouns_list.remove("he")
-    assert_equal ["him"], @person.pronouns_list.to_a
+    @car.colors_list.add("red, blue")
+    @car.colors_list.remove("red")
+    assert_equal ["blue"], @car.colors_list.to_a
   end
 
   def test_clear_tags
-    @person.pronouns_list.add("he/ him")
-    @person.pronouns_list.clear
-    assert_equal [], @person.pronouns_list.to_a
+    @car.colors_list.add("red, blue")
+    @car.colors_list.clear
+    assert_equal [], @car.colors_list.to_a
   end
 
   def test_include_tag
-    @person.pronouns_list.add("he/ him")
-    assert @person.pronouns_list.include?("he")
-    assert_not @person.pronouns_list.include?("she")
+    @car.colors_list.add("red, blue")
+    assert @car.colors_list.include?("red")
+    assert_not @car.colors_list.include?("green")
   end
 
   def test_empty_tags
-    assert @person.pronouns_list.empty?
-    @person.pronouns_list.add("he")
-    assert_not @person.pronouns_list.empty?
+    assert @car.colors_list.empty?
+    @car.colors_list.add("red")
+    assert_not @car.colors_list.empty?
   end
 
   def test_add_tags!
-    @person.pronouns_list.clear
-    @person.pronouns_list.add!("he/ him")
-    assert_equal ["he", "him"], @person.pronouns_list.to_a
+    @car.colors_list.clear
+    @car.colors_list.add!("red, blue")
+    assert_equal ["red", "blue"], @car.colors_list.to_a
   end
 
   def test_remove_tags!
-    @person.pronouns_list.clear
-    @person.pronouns_list.add!("he/ him")
-    @person.pronouns_list.remove!("he")
-    assert_equal ["him"], @person.pronouns_list.to_a
+    @car.colors_list.clear
+    @car.colors_list.add!("red, blue")
+    @car.colors_list.remove!("red")
+    assert_equal ["blue"], @car.colors_list.to_a
   end
 
   def test_clear_tags!
-    @person.pronouns_list.add!("he/ him")
-    @person.pronouns_list.clear!
-    assert_equal [], @person.pronouns_list.to_a
+    @car.colors_list.add!("red, blue")
+    @car.colors_list.clear!
+    assert_equal [], @car.colors_list.to_a
+  end
+
+  def test_add_tags_as_array
+    @car.colors_list.add(["red", "blue"])
+    @car.colors_list.add(["green", "yellow"])
+    assert_equal ["red", "blue", "green", "yellow"], @car.colors_list.to_a
+
+    @car.colors_list.remove(["red", "blue", "green"])
+    assert_equal ["yellow"], @car.colors_list.to_a
   end
 end
